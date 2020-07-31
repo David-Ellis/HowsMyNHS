4516c98926f6a4d5ca5d48a6df891eb0b35b4c41
David-Ellis <david.ellis94@gmail.com>
2020-07-31 14:12:24 +0200

    wrote function to generate trust pages

--- build_website.py
+++ build_website.py
@@ -111,6 +111,148 @@ def MakeHomepage(data):
     
     print("Done")
     
+def build_trust_pages(data):
+    print("Building trust pages...", end = " ")
+    # Load data
+    NHSdata = np.load(data, allow_pickle=True)
+    names = NHSdata[0]
+    dates = dates2num(NHSdata[1])
+    attendance = NHSdata[2]
+    waiting = NHSdata[3]
+
+    for i, name in enumerate(names):
+        mask = (waiting[i,:] != '-')
+
+        pageExist = False
+        if sum(mask)>12:
+            pageExist = True
+
+        # print(i,name, pageExist)
+
+        if i == 0 and pageExist:
+            url_prefix = '_'.join(name.lower().split(' '))
+            url = ''.join([url_prefix,".html"])
+            file = open("hospitals/{}".format(url), "w")
+            figName = '_'.join(name.lower().split(' '))
+            path = "../figures/{}.png".format(figName)
+            #print("figures/{}.png".format(figName))
+            subTitleHTML = '''<div class = \"box\">
+            \n<h2 class = \"subtitle\"><center>All of England</center></h2>\n'''
+
+            imgHTML = "<center><img src=\"{}\" alt=\"{}\"></center>".format(path, name)
+
+            supTextHTML = u'''
+
+                <p>After nearly a decade of Conservative rule, in December over <b>500,000</b> more people were made to wait
+                <b>over four hours</b> to be seen at A&E than in 2011. That's an increase of over <b>900%</b>.
+                The number of people attending A&E, however, had only increased by less than 30%.<p>
+
+                {}
+
+                <p>Now, Brexit and a trade deal with Trump's US threatens to increase the NHS drug bill from &pound18 billion to as much 
+                as <b>&pound45 billion</b> a year while shutting out the nurses, carers and other workers that the health service depends on</p>
+
+                <p>Over the last few months this number has decreased dramatically. However, this is because the number of people attending
+                A&E has fallen by more than 50% as people choose to stay at home to help reduce the pressure on the NHS 
+                during the current pandemic.</p>            
+
+                \n</div>'''.format(imgHTML)
+
+
+            file.write(''.join([headHTML,subTitleHTML,supTextHTML,whatNextHTML,tailHTML]))
+            file.close() 
+        elif sum(attendance[i,:] != '-')>=12 and pageExist:
+            url_prefix = '_'.join(name.lower().split(' '))
+            url = ''.join([url_prefix,".html"])
+            file = open("hospitals/{}".format(url), "w")
+            figName = '_'.join(name.lower().split(' '))
+            path = "../figures/{}.png".format(figName)
+            #print("figures/{}.png".format(figName))
+            subTitleHTML = '''\n<div class = \"box\">
+            <h2 class = \"subtitle\"><center>{}</center></h2>\n'''.format(name)
+
+            imgHTML = "<center><img src=\"{}\" alt=\"{}\"></center>".format(path, name)
+
+            smoothWait = movingAverage(waiting[i,:][waiting[i,:] != '-'])
+            avAtt = np.mean(attendance[i,:][attendance[i,:] != '-'])
+            sampleDates = dates[waiting[i,:] != '-']
+            if max(smoothWait) < 15 and avAtt<2000 and pageExist:
+                supTextHTML = '''
+                <!--Minimal Change Hospital + less than 2000 monthy attendance-->
+
+                <p>It looks like things haven't changed much for your hospital! On average only {} people attend this A&E 
+                each month.</p>
+                <p>Unfortunately, things aren't so great for the rest of England. After nearly a decade of Conservative rule,
+                each month over <b>400,000</b> more people are made to wait <b>over four hours</b> to be seen at A&E than 
+                in {}.
+
+                {}
+
+                <p>Now, Brexit and a trade deal with Trump's US threatens to increase the NHS drug bill from &pound18 billion to as much 
+                as <b>&pound45 billion</b> a year while shutting out the nurses, carers and other workers that the health service depends on</p>
+
+                <p>Note: Over the past three months A&E attendance across England has fallen by more than 50% as people choose to stay at home to help reduce the pressure on the NHS 
+                during the current pandemic.</p>
+
+                \n</div>'''.format(int(avAtt), int(round(min(sampleDates))), imgHTML)
+
+            elif max(smoothWait) < 15 and pageExist:
+                supTextHTML = '''
+                <!--Minimal Change Hospital + more than 2000 monthy attendance-->
+
+                <p> It looks like things haven't changed much for your hospital!</p>
+                <p>Unfortunately, things aren't so great for the rest of England. After nearly a decade of Conservative rule,
+                each month over <b>400,000</b> more people are made to wait <b>over four hours</b> to be seen at A&E than in 2011.
+
+                {}
+
+                <p>Now, Brexit and a trade deal with Trump's US threatens to increase the NHS drug bill from &pound18 billion to as much 
+                as <b>&pound45 billion</b> a year while shutting out the nurses, carers and other workers that the health service depends on.</p>
+
+                <p>Note: Over the past three months A&E attendance across England has fallen by more than 50% as people choose to stay at home to help reduce the pressure on the NHS 
+                during the current pandemic.</p>
+                \n</div>'''.format(imgHTML)
+
+            elif avAtt>2000 and smoothWait[0]-smoothWait[-1]>100 and pageExist:
+                diff = smoothWait[0]-smoothWait[-1]
+                supTextHTML = '''
+                <p>After nearly a decade of Conservative rule, on average, <b>{}</b> more people are being left to wait over 
+                four hours at A&E at <b>your</b> hospital than back in {}.</p>
+
+                {}
+
+                <p>And things are bad for the rest of England too. Each month over <b>400,000</b> more people are made to wait
+                <b>over four hours</b> to be seen at A&E than in 2011.
+
+                <p>Now, Brexit and a trade deal with Trump's US threatens to increase the NHS drug bill from &pound18 billion to as much 
+                as <b>&pound45 billion</b> a year while shutting out the nurses, carers and other workers that the health service depends on</p>
+
+                <p>Note: Over the past three months A&E attendance across England has fallen by more than 50% as people choose to stay at home to help reduce the pressure on the NHS 
+                during the current pandemic.</p>
+                \n</div>'''.format(int(diff),int(np.floor(min(sampleDates))), imgHTML)
+            elif pageExist:
+                #print(name)
+                supTextHTML = '''
+                <p> The data show's that thing's have gotten worse in your hospital. With {} more people each month 
+                 being forced to wait over 4 hours to be seen at A&E since {}. It many English hospitals, the situation is much
+                 worse. After nearly a decade of Conservative rule, each month over <b>400,000</b> more people are made 
+                 to wait <b>over four hours</b> to be seen at A&E than in 2011.</p>
+
+                 {}
+
+                <p>Now, Brexit and a trade deal with Trump's US threatens to increase the NHS drug bill from &pound18 billion to as much 
+                as <b>&pound45 billion</b> a year while shutting out the nurses, carers and other workers that the health service depends on</p>
+
+                <p>Note: Over the past three months A&E attendance across England has fallen by more than 50% as people choose to stay at home to help reduce the pressure on the NHS 
+                during the current pandemic.</p>
+                \n</div>
+                '''.format(int(diff), int(np.floor(min(sampleDates))), imgHTML)
+
+
+
+            file.write(''.join([headHTML,subTitleHTML,supTextHTML,whatNextHTML,tailHTML]))
+            file.close() 
+    print("Done.")
     
 ####################################################################################################
 ########################################### Website Text ########################################### 
@@ -189,7 +331,105 @@ function myFunction() {
 </html>
 ''' 
 
+headHTML = '''
+<html>
+<head>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154345093-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154345093-1');
+</script>
+
+
+<title>How's my NHS?</title>
+<link rel="stylesheet" type="text/css" href="../style.css">
+
+</head>
+<body>
+<div class="maintitle">
+<h1><center><a href="../index.html"><img src="../logo.png" alt="How's my NHS?" /></a></center></h1>
+</div>'''
+
+tailHTML = '''
+</body>
+</html>'''
+
+whatNextHTML = '''
+    </br>
+    <div class ="box">
+    <h2 class = \"subtitle\"><center>What Next?</center></h2>\n
+    
+    <p><b style="color:#005EB8;">Prevent the spread:</b> With the outbreak of the Coronavirus (COVID-19)
+    the NHS is under more pressure than ever. We <b>need</b> to do everything we can to slow its
+    spread. Follow the official government guidance: 
+    <ul>
+    <li>Stay at home as much as possible</li>
+    <li>Work from home if you can</li>
+    <li>Limit contact with other people</li>
+    <li>Keep your distance from people not in your household (2 metres apart where possible)</li>
+    <li>Wash your hands regularly</li>
+    
+    </ul>
+    Many young people experience few to no symptoms at all but can still carry the virus and pass 
+    it on to those who are more vulnerable.
+    </p>
+    <p>
+    Stay informed with reliable sources such as the governments 
+    <a href="https://www.gov.uk/coronavirus" target="_blank">offical Coronavirus page</a>.
+    </p> 
+
+    <p><b style="color:#005EB8;">Share the message:</b> Fight for change. Let your friends and family know that the Conservatives 
+    are not the party for the NHS. One vote may have little impact, particularly with our broken electoral system. 
+    But together we can bring about great change.</p> 
+
+<div id="share-buttons" align="center" style="color:#ffffff;">
+    
+    <!-- Twitter -->
+    <a href="https://twitter.com/share?url=https://howsmynhs.co.uk/&amp;text=How%20has%20YOUR%20NHS%20changed%20under%20the%20Tories?&amp;hashtags=NHS"target="_blank">
+    <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" /></a>
+    
+    <!-- Email -->
+    <a href="mailto:?Subject=Simple Share Buttons&amp;Body=How%20has%20YOUR%20NHS%20changed%20under%20the%20Tories?%20 https://howsmynhs.co.uk/">
+    <img src="https://simplesharebuttons.com/images/somacro/email.png" alt="Email" /></a>
+ 
+    <!-- Facebook -->
+    <a href="http://www.facebook.com/sharer.php?u=https://howsmynhs.co.uk/" target="_blank">
+    <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" /></a>
+    
+    <!-- Reddit -->
+    <a href="http://reddit.com/submit?url=https://howsmynhs.co.uk/&amp;title=How has your NHS changed under the Tories?" target="_blank">
+    <img src="https://simplesharebuttons.com/images/somacro/reddit.png" alt="Reddit" /></a>
+
+    <!-- Tumblr-->
+    <a href="http://www.tumblr.com/share/link?url=https://howsmynhs.co.uk/.com&amp;title=Simple Share Buttons" target="_blank">
+    <img src="https://simplesharebuttons.com/images/somacro/tumblr.png" alt="Tumblr" /></a>
+    
+    <!-- LinkedIn -->
+    <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=https://howsmynhs.co.uk/" target="_blank">
+    <img src="https://simplesharebuttons.com/images/somacro/linkedin.png" alt="LinkedIn" /></a>
+    
+   <! -- Share buttons from: https://simplesharebuttons.com/html-share-buttons/ --!>
+</div>
+
+    <p><b style="color:#005EB8;">Email your MP:</b> Enter your postcode 
+    <a href="https://members.parliament.uk/" target="_blank">here</a>.
+    to find the email address for your local MP. Send them this graph of 
+    how <b>your</b> local A&E has changed and urge them to use their power to
+    protect it from further damage and to restore our NHS to it's former 
+    glory.
+    </p>
+
+    <p><b style="color:#005EB8;">Use this data:</b> All of the data presented here, as well as the code used to collect it, is <a href="https://github.com/David-Ellis/NHSdata" target="_blank"> freely availble via github</a>. 
+    You don't need to cite us. This is all publically available data from <a href="https://www.england.nhs.uk/statistics/statistical-work-areas/ae-waiting-times-and-activity/" target="_blank">NHS England</a>. 
+    </p>    
+    
+    </br></br></br></br></br></br></br></br>
+    '''
 
 
 
