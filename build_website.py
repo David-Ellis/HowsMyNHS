87d6b88365bd1c849df3a75ad05d9b922fa2b6c7
David-Ellis <david.ellis94@gmail.com>
2020-07-31 14:04:20 +0200

    Added function to build homepage to module

--- build_website.py
+++ build_website.py
@@ -81,3 +81,122 @@ def plotWaitingData(data):
                 plt.close()
                 
     print("Done.")
+    
+def MakeHomepage(data):
+    print("Building homepage...", end = " ")
+    
+    # Load data
+    NHSdata = np.load(data, allow_pickle=True)
+    names = NHSdata[0]
+    dates = dates2num(NHSdata[1])
+    attendance = NHSdata[2]
+    waiting = NHSdata[3]
+    
+    # Make list of hospital names
+    hospitalLinksList = []
+    for i, name in enumerate(names):
+            if type(name) == str:
+                mask = (waiting[i,:] != '-')
+                if sum(mask)>10:
+                    url_prefix = '_'.join(name.lower().split(' '))
+                    url = ''.join(["hospitals/",url_prefix,".html"])
+                    hospitalLinksList.append("<li><a href=\"{}\">{}</a></li>\n".format(url,name))
+
+    hospitalLinks = ''.join(hospitalLinksList)
+    file = open("index.html", "w") 
+
+    file.write(''.join([homeHTML1, hospitalLinks, homeHTML2])) 
+ 
+    file.close() 
+    
+    print("Done")
+    
+    
+####################################################################################################
+########################################### Website Text ########################################### 
+####################################################################################################
+
+siteURL = "https://howsmynhs.co.uk/"
+
+homeHTML1 = '''
+<html>
+<head>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-154345093-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-154345093-1');
+</script>
+
+<title>How's my NHS?</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<div class="maintitle">
+<h1><center><a href="index.html"><img src="logo.png" alt="How's my NHS?" /></a></center></h1>
+</div>
+
+<center>
+<div class="searchbox">
+<div class="description">
+<p style="padding: 0px; margin: 0px;">The number of people waiting over 4 hours at A&E in England has increase by over 900%.
+How is your local NHS Trust doing?</p>
+</div>
+
+<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for name...">
+</br>
+
+</br></br>
+<ul id="myUL">'''
+
+homeHTML2 = '''
+</ul> 
+</div>
+
+<script>
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    if(input.value.length == 0){
+        ul.style.display = "none";
+        return;
+    }else{
+        ul.style.display = "block";
+    }
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+</script>
+
+</body>
+</html>
+''' 
+
+
+
+
+
+
+
+
+
+
+
+
